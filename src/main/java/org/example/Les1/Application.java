607a27316baec5c58e7959e3f6516a15d46bc0b5
“Mihail <mic311089@gmail.com>
2023-08-22 12:51:22 +0400

    1

---
+++ src/main/java/org/example/Les1/Application.java
@@ -0,0 +1,25 @@
+package org.example.Les1;
+
+import java.io.File;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(getFileSize(new File("123")));
+        System.out.println(divide(10, 0));
+    }
+
+    public static int divide(int a1, int a2) {
+        if (a2 == 0) {
+//            return -1;
+            throw new RuntimeException("Divide by zero not permitted");
+        }
+        return a1 / a2;
+    }
+
+    public static long getFileSize(File file) {
+        if (!file.exists()) {
+            return -1L;
+        }
+        return file.length();
+    }
+}
